53c0ac85bbd0957602441b4772c61aa45cf4f322
Martin Zikmund <martin@zikmund.dev>
2024-01-24 14:34:53 +0100

    chore: Progress porting KeyboardAcceleratorUtility

--- src/Uno.UI/UI/Xaml/UIElement.cs
+++ src/Uno.UI/UI/Xaml/UIElement.cs
@@ -502,7 +502,7 @@ namespace Microsoft.UI.Xaml
 		}
 
 		[NotImplemented]
-		protected virtual AutomationPeer OnCreateAutomationPeer() => new AutomationPeer();
+		protected internal virtual AutomationPeer OnCreateAutomationPeer() => new AutomationPeer();
 
 		internal AutomationPeer OnCreateAutomationPeerInternal() => OnCreateAutomationPeer();
 
